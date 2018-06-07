857fb6a35f8195bc3c5edf8953a213d14cd27581
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-06-07 16:39:47 -0300

    Fazendo a Thread principal aguardar

---
+++ src/br/com/alura/threads/Principal.java
@@ -0,0 +1,13 @@
+package br.com.alura.threads;
+
+public class Principal {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.out.println("isso é o método main");
+		
+		
+		Thread.sleep(60000);
+	}
+
+}
