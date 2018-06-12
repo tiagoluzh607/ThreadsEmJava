695e3b89604774c815a764e47ff249b1bf02d29f
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-06-12 10:56:02 -0300

    Fazendo varios threads manipularem uma lista apenas

---
+++ src/br/com/alura/lista/Lista.java
@@ -0,0 +1,22 @@
+package br.com.alura.lista;
+
+public class Lista {
+
+	
+	private String[] elementos = new String[10000];
+	private int indice = 0;
+	
+	public void adicionaElementos(String elemento) {
+		this.elementos[indice] = elemento;
+		this.indice++;
+	}
+	
+	public int tamanho() {
+		return this.elementos.length;
+	}
+	
+	public String pegaElemento(int posicao) {
+		return this.elementos[posicao];
+	}
+	
+}
